d118db6f5291fe70e837ca8608881d1f9bcf9f50
jornveltrop <jorn1veltrop@gmail.com>
2022-05-09 11:02:13 +0200

    Deploy Heroku

--- auth.js
+++ auth.js
@@ -8,7 +8,7 @@ const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET
 passport.use(new GoogleStrategy({
     clientID:     GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
-    callbackURL: "http://localhost:8000/google/callback",
+    callbackURL: "https://little-list-jorn.herokuapp.com/google/callback",
     passReqToCallback   : true
   },
   function(request, accessToken, refreshToken, profile, done) {
